cb4b6599b6417892f34a8e02987f1895fbee6a72
YPermitin <ypermitin@yandex.ru>
2020-04-16 13:03:33 +0500

    Актуализировал версию сборки и скрипт доставки

--- Scripts/deploy.sh
+++ Scripts/deploy.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-#dotnet nuget push
+API_KEY = $1
 
-ApiKey = $1
-echo "API KEY: $1"
+dotnet nuget push ./YY.EventLogReaderAssistant/bin/Release/YY.EventLogReaderAssistant.*.nupkg -k $1 -s https://api.nuget.org/v3/index.json
